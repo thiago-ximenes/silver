2e7385186f752f610660cb3f996c0f038bbecdd0
ximenes <thiago@ximenes.dev>
2024-09-18 18:53:35 -0300

    fix(environment): remove log error message from unknown role

--- silver-frontend/src/schema/fields/password.field.schema.ts
+++ silver-frontend/src/schema/fields/password.field.schema.ts
@@ -5,5 +5,5 @@ export const passwordFieldSchema = z
     required_error: "Password é obrigatório",
     invalid_type_error: "Password precisa ser preenchido",
   })
-  .min(8, "Password precisa ter no mínimo 6 caracteres")
+  .min(8, "Password precisa ter no mínimo 8 caracteres")
   .max(30, "Password precisa ter no máximo 30 caracteres");
